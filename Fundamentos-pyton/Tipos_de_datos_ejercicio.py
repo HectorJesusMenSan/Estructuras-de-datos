2305845651b053276120907ca3f5567cfe17999a
SC2 <hectorjesusm08@gmail.com>
2024-10-10 15:19:13 -0600

    Se crearon los programas tipos de datos y su ejerciscio

---
+++ Fundamentos-pyton/Tipos_de_datos_ejercicio.py
@@ -0,0 +1,23 @@
+"""
+Hector Jesus Mendez Santiago.
+9 de octubre de 2024.
+Descripcion: este programa contiene el ejercicio
+que indica la leccion de los tipos de datos.
+"""
+dia1 = "lunes"
+gasto_pasaje1 = 20
+gasto_comida1 = 135
+otros_gastos1= 34.5
+real_bool1 = True
+
+dia2 = "lunes"
+gasto_pasaje2 = 20
+gasto_comida2 = 135
+otros_gastos2= 34.5
+real_bool2 = False
+print("*** Gastos diarios ***")
+print("dia:", dia1)
+print("pasaje:", gasto_pasaje1)
+print("comida:", gasto_comida1)
+print("otros gastos:", otros_gastos1)
+print("¿fue a mayor a mi presupuesto?", real_bool1)
---
+++ Fundamentos-pyton/Tipos_de_datoss.py
@@ -0,0 +1,48 @@
+'''
+Hector Jesus Mendez Santiago
+9 de octubre de 2024.
+Descripción:
+En este programa estan los tipos de variables en python y su forma en las que se usan
+'''
+
+# Notas:
+"""
+En Python, no es necesario declarar el tipo de dato al declarar una variable.
+Los valores automaticos, sin la necesidad de declarar con los que se puede trabajar en python son todos los siguientes:
+- Int (numeros enteros)
+- Float (numeros con punto decimal)
+- String (str) (cadenas de texto automaticamente)
+- Boolean. True o False (con inicial mayúscula).
+- None. Tipo especial que representa una ausencia de valor.
+"""
+
+# Ejemplos de tipos de datos.
+
+# Número entero
+mi_variable_entera = -100
+print("Tipo de dato entero:",mi_variable_entera)
+
+# Número decimal
+mi_variable_decimal = 12.12
+print("Tipo de dato decimal:", mi_variable_decimal)
+
+# Cadena de texto
+mi_variable_texto_nombre = 'Hector'   #Al usar las dos maneras de comillas no afecta en la escritura
+mi_variable_texto_apellido = "Mendez" #automaticamente imprime los dos textos de manera separada
+print("Cadena de texto:", mi_variable_texto_nombre, mi_variable_texto_apellido)
+
+# Booleno
+mi_variable_booleana = True
+print('Tipo booleano:', mi_variable_booleana)
+
+# None
+mi_variable_none = None
+print("Tipo none:",mi_variable_none)
+
+# Uso de constantes.
+'''
+En Python, a diferencia de otros lenguajes de programación, no existe un tipo específico para definir constantes.
+Se utiliza una convención de colocar las variables en mayúsculas y no modificarlas.
+'''
+VARIABLE_CONSTANTE = 3.1416 #el nombre de una variable constante debe ser codificada con todas las letras mayusculas
+print("Ejemplo de convención de una constante:", VARIABLE_CONSTANTE)
