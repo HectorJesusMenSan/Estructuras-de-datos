8d9642259fcc0a112c4628f725e892df95c7a962
SC2 <hectorjesusm08@gmail.com>
2024-10-16 16:00:56 -0600

    Comentar codigos, investigar e intentar hacer la tarea

--- Operadores-en-Python/Operadores_logicos.py
+++ Operadores-en-Python/Operadores_logicos.py
@@ -1,1 +1,4 @@
 #pedir dos expresiones y que al insertar si o no nos devuelva si o no debe de debolber true o false
+variable_logico=input("ingresa si o no: ")
+variable_logico=variable_logico.lower()=="si"
+print(variable_logico)
