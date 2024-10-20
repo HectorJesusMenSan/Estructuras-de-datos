82bf8438bf8bb6e8f5069db9a7cea6a34537f55d
jesusito personal <hectorjesusm08@gmail.con>
2024-10-19 21:54:39 -0600

    Listos

--- Operadores-en-Python/operadores_ejercicio3.py
+++ Operadores-en-Python/operadores_ejercicio3.py
@@ -12,6 +12,6 @@ se encuentra en un rango de: 10-15.
 
 """
 
-print(f"\t###INICIANDO PROGRAMA###")                                            #   Muestra en pantalla un pequeño letrero.
-numero=int(input("Ingresa un número: "))                                        #   Solicitud y conversión de datos.
-print(f"El número: {numero} esta entre 10 y 15? {numero>=10 and numero<=15}")   #   Muestra el resultado si el numero esta en el rango de 10-15.
+print(f"\t###INICIANDO PROGRAMA###")                                            #Muestra en pantalla un pequeño letrero.
+numero=int(input("Ingresa un número: "))                                        #Solicitud y conversión de datos.
+print(f"El número: {numero} esta entre 10 y 15? {numero>=10 and numero<=15}")   #Muestra el resultado si el numero esta en el rango de 10-15.
