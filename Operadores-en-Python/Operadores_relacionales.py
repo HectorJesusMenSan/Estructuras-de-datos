00832bb0d12f720584ba8ba629b83ef82ad37b08
jesusito personal <hectorjesusm08@gmail.con>
2024-10-27 17:58:19 -0600

    Listos

--- Operadores-en-Python/Operadores_relacionales.py
+++ Operadores-en-Python/Operadores_relacionales.py
@@ -5,15 +5,15 @@ Fecha:          16 de octubre de 2024.
 Descripción:    En este programa se abordan los temas de operadores relacionales
                 que se pueden usar en Python: ==, <, >, !=.
 """
-#   Al igual que en c y c++ usamos los mismos operadores o las mismas nomenlaturas.
+#   Al igual que en c y c++ usamos los mismos operadores o las mismas nomenclaturas.
 #   Se utilizan para comparar dos valores.
-numero1, numero2 = float(input("escribir numero 1: ")), float(input("escribir numero 2: "))     #   Solicitud, lectura y conversion de datos a flotantes.
+numero1, numero2 = float(input("Escribir número 1: ")), float(input("Escribir número 2: "))     #   Solicitud, lectura y conversion de datos a flotantes.
 print(f"¿El número {numero1:.2f} es mayor que {numero2:.2f}? {numero1>numero2}")                #   Muestra en resultado booleano si el numero1 es mayor al numero2.
 print(f"¿El número {numero1:.2f} es mayor o igual que {numero2:.2f}? {numero1>=numero2}")       #   Muestra en resultado booleano si el numero1 es mayor o igual al numero2.
-print(f"¿El número {numero1:.2f} es igual que {numero2:.2f}? {numero1==numero2}")               #   Muestra en resultado booleano si ambos numeros son iguales.
+print(f"¿El número {numero1:.2f} es igual que {numero2:.2f}? {numero1==numero2}")               #   Muestra en resultado booleano si ambos números son iguales.
 print(f"¿El número {numero1:.2f} es menor que {numero2:.2f}? {numero1<numero2}")                #   Muestra en resultado booleano si el numero1 es menor al numero2.
 print(f"¿El número {numero1:.2f} es menor o igual que {numero2:.2f}? {numero1<=numero2}")       #   Muestra en resultado booleano si el numero1 es menor o igual al numero2.
-print(f"¿El número {numero1:.2f} es diferente que {numero2:.2f}? {numero1!=numero2}")           #   Muestra en resultado booleano si ambos numeros son diferentes.
+print(f"¿El número {numero1:.2f} es diferente que {numero2:.2f}? {numero1!=numero2}")           #   Muestra en resultado booleano si ambos números son diferentes.
 
 """
 Los operadores relacionales son símbolos que se utilizan en programación para comparar dos valores. 
