22329db0c790eb20b101f3d1f93380df9a62baa5
jesusito personal <hectorjesusm08@gmail.con>
2024-11-24 13:54:14 -0600

    Listos para enviar, programas ya corregidos

--- Ciclos/Ciclo_while.py
+++ Ciclos/Ciclo_while.py
@@ -1,35 +1,49 @@
 """
 Nombre:         Héctor Jesús Méndez Santiago.
 Fecha:          28 de octubre de 2024.
-Descripción:    ciclos.
+Descripción:    Ciclos.
+                Programa de introducción
+                al funcionamiento de los ciclos
+                while en Python.
 """
 
-numero_registrado = int(input("Ingresa un numero: "))
-contador = 1
 
-print(f"los numero del 1 al {numero_registrado}")
+# Declaración de variables:
+numero_registrado = int(input("Ingresa un número: "))               # Solicitud de datos.
+contador = 1                                                        # Se inicializa un contador.
+
+
+print(f"Los números del 1 al {numero_registrado}")                   # Letrero informativo.
+
+# Proceso de sumatoria, condición: hasta que el contador sea igual al número registrado.
 while contador <= numero_registrado :
     print(contador)
-    contador+=1                                     #No hay contadores unitarios
+    contador+=1                                                     # No hay contadores unitarios
 
-#Segundo Ejercicio
-numero_registrado2 = int(input("Ingresa un numero: "))
+"""-El ciclo while, requiere una condición, en la
+    que esta itera mientras esta sea verdadera.
+"""
+
+# Segundo ejercicio
+numero_registrado2 = int(input("Ingresa un número: "))
 contador2 = 1
 
-print(f"los numero del 1 al {numero_registrado2}")
+print(f"Los números del 1 al {numero_registrado2}")
+
 while contador2 <= numero_registrado2 :
-    print(contador2, end= " ")
+    print(contador2, end= " ")                          # Palabra reservada para imprimir en una sola línea.
     contador2+=1
 print("\nFin de cuenta, fuera del ciclo.")
 
 
-#tercer ejercicio:
-#imprimir todos los numero pares hasta el numero solicitado
-numero_de_usuario = int(input("ingresa un  numero cualquiera: "))
+# Tercer ejercicio:
+# Imprimir todos los números pares hasta el número solicitado
+numero_de_usuario = int(input("Ingresa un número cualquiera: "))
 contador3 = 1
 
-print(f"los numeros pares de 1 a {numero_de_usuario} son: ")
-while contador3<=numero_de_usuario :
-    if contador3 %2 == 0 :
+print(f"Los números pares de 1 a {numero_de_usuario} son: ")
+while contador3 <= numero_de_usuario :
+    if contador3 % 2 == 0 :
+        # Si el residuo de la división entre 2 es cero imprime el número.
         print(contador3, end=" ")
-    contador3+=1
+    contador3 += 1
--- Ciclos/Ciclos_ejercicio1.py
+++ Ciclos/Ciclos_ejercicio1.py
@@ -1,17 +1,41 @@
 """
 Nombre:         Héctor Jesús Méndez Santiago.
 Fecha:          28 de octubre de 2024.
-Descripción:    ciclos.
+Descripción:    Primer ejercicio, para practicar los
+                ciclos, en este caso el while:
+                El ejercicio consiste en calcular la suma
+                acumulativa, de 0 hasta el número que
+                indique el usuario del programa.
 """
 
-"""programa que calcula la suma acomulativa"""
+# Escritura de letreros interactivos.
+"""Programa que calcula la suma acumulativa"""
+print("#Cargando programa: Suma acumulativa#")
+print("-_-")
 
-numero_ingresado = int(input("ingresa un numero que marque el final: "))
-contador=0
-registro_de_suma=0
+# Declaración de variables y solicitud de datos.
+numero_ingresado = int(input("¿Hasta qué número deseas que termine la suma acumulativa? :) : "))
+contador = 1
+registro_de_suma = 0
 
-print(f"la suma de 0 hasta {numero_ingresado} es: ")
+print(f"La suma acumulativa de 0 hasta {numero_ingresado} es: ")
 while contador <= numero_ingresado :
-    registro_de_suma=registro_de_suma+contador
-    contador+=1
-print(registro_de_suma)
+    registro_de_suma = registro_de_suma + contador
+    print(f"{registro_de_suma - contador} + {contador} = {registro_de_suma}")
+    contador += 1
+print(f"El resultado final es: {registro_de_suma}")
+
+"""-El funcionamiento del while consiste en iniciar el contador en 1,
+    y la condición es que mientras el contador sea menor o igual que
+    el número ingresado por el usuario, aumentará el contador. Pero antes
+    de la acumulación, debe realizarse una suma. Se inicializa una variable
+    que lleve el registro de la suma y esta misma se actualiza constantemente
+    mientras que el acumulador aumenta. La suma consiste en sumar
+    el registro de suma por el valor que tiene el contador en cada iteración y
+    guardándola en sí misma para acumularla. Cabe aclarar que la variable para
+    el registro de la suma debe inicializarse en 0.
+    Al final, solo se imprime el registro de la suma, que se acumuló en el 
+    ciclo hasta que la condición se rompa.
+"""
+
+print("\nF I N  D E  P R O G R A M A")
