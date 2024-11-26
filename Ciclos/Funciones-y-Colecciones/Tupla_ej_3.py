0c1297759c0f443f8c3dc1c0b445ba431e4b21ee
jesusito personal <hectorjesusm08@gmail.con>
2024-11-25 23:46:11 -0600

    Listos para enviar, programas ya corregidos

---
+++ Ciclos/Funciones-y-Colecciones/Tupla_ej_3.py
@@ -0,0 +1,13 @@
+"""
+Nombre:         Héctor Jesús Méndez Santiago.
+Fecha:          19 de noviembre de 2024.
+Descripción:   Ejercicios para practicar
+               Con el tema de tuplas"""
+def menu():
+    print("\n1) Ver coordenadas almacenadas.")
+    print("2) Agregar coordenada (x,y).")
+    print("3) Calcular la pendiente y la ecuación de la recta entre dos coordenadas.")
+    print("4) Eliminar coordenada (x,y).")
+    print("0) Salir.")
+    opcion = int(input("Escoege opcion: "))
+    return opcion
