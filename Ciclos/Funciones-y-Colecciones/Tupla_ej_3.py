8861573a305918f7b03f163c0afe8250cbdd3099
SC2 <hectorjesusm08@gmail.com>
2024-11-28 14:55:22 -0600

    corregir ortografia

--- Ciclos/Funciones-y-Colecciones/Tupla_ej_3.py
+++ Ciclos/Funciones-y-Colecciones/Tupla_ej_3.py
@@ -2,12 +2,29 @@
 Nombre:         Héctor Jesús Méndez Santiago.
 Fecha:          19 de noviembre de 2024.
 Descripción:   Ejercicios para practicar
-               Con el tema de tuplas"""
-def menu():
-    print("\n1) Ver coordenadas almacenadas.")
-    print("2) Agregar coordenada (x,y).")
-    print("3) Calcular la pendiente y la ecuación de la recta entre dos coordenadas.")
-    print("4) Eliminar coordenada (x,y).")
-    print("0) Salir.")
-    opcion = int(input("Escoege opcion: "))
-    return opcion
+               Con el tema de tuplas
+               rol de juegos"""
+from Ciclos.Ciclo_while import contador
+
+
+def agregaar_equipos(lista_de_equipos):
+    print("DEBES INGRESAR UNA CANTIDAD PAR...")
+    cantidad_de_equipos=int(input("Cuantos equipos se escribieron? "))
+    for i in range(0, cantidad_de_equipos):
+        lista_de_equipos.append(f"equipo{i+1}")
+    return lista_de_equipos
+
+def jornadas_para_juego (lista_de_equipos):
+    lista_de_tuplas = []
+    tupla_de_equipos = ()
+    cantidad_de_jornadas = int(input("Cuantas jornadas de juego organizaras? "))
+    contador_de_lista = len(lista_de_equipos)
+    for iterador in range(1, contador_de_lista-1):
+        auxiliar=lista_de_equipos[iterador]
+        lista_de_equipos=[iterador+1]
+
+
+
+
+
+print("Cargando programa: Rol de juegos")
