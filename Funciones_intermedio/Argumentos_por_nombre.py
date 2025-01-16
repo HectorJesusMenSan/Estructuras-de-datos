2018e75a03f5755d4073b6710c7450e0b09fed40
SC2 <hectorjesusm08@gmail.com>
2025-01-16 15:45:42 -0600

    corregir ortografia

--- Funciones_intermedio/Argumentos_por_nombre.py
+++ Funciones_intermedio/Argumentos_por_nombre.py
@@ -3,15 +3,16 @@ Nombre: Héctor Jesús Méndez Santiago
 Fecha: 15 de enero del 2025
 Descripción: Bibliotecas como en c
 """
-def imprimir_alumno (nombre: str, edad: int, matricula: str, grupo: int, semestre: int) -> None:
+#Despues del asterisco se tiene que escribir forzosamente el mismo nombre en la llamada.
+def imprimir_alumno (nombre: str, edad: int, matricula: str, grupo: int,*, semestre: int= 3) -> None: #Inicializa valores en funcion, por si no le mandamos nada en el menu.
     """
     Funcion que imprime los dats personales del usuario
-    :param nombre:
-    :param edad:
-    :param matricula:
-    :param grupo:
-    :param semestre:
-    :return:
+    :param nombre: Nombre de un usuario.
+    :param edad: Edad del usuario.
+    :param matricula: Matricula delalumno.
+    :param grupo: Grupo del alumno.
+    :param semestre: Semestre que cursa el alumno.
+    :return: No retorna nada.
     """
     print("-Estos son los datos personales: \n")
     print(f"-Nombre:  {nombre}\n"
@@ -23,7 +24,15 @@ def imprimir_alumno (nombre: str, edad: int, matricula: str, grupo: int, semestr
 
 def main() -> None:
     nombre, edad, matricula, grupo, semestre = "Hector", 19, 123213123, 303, 3
-    imprimir_alumno(nombre, edad, matricula, grupo, semestre)
+    imprimir_alumno(nombre, edad, matricula,grupo ,semestre )
+
+    #Manda "Argumentos por nombre"
+    imprimir_alumno(nombre="Albertano", edad=19, matricula=123232, semestre="4", grupo="303")
+
+    #Despues del aterisco le tenemos llevar la misma palabra sino no agarra.
+    imprimir_alumno("jss", 31, "123", 3,  semestre = 12 )
+
+
 
 if __name__ == '__main__':
     main()
