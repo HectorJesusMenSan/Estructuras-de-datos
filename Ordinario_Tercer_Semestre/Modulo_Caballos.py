5de9f406d1e61a081761b801455a61ddd309c218
jesusito personal <hectorjesusm08@gmail.con>
2025-02-10 11:11:47 -0600

    Listos para enviar, programas ya corregidos

--- Ordinario_Tercer_Semestre/Modulo_Caballos.py
+++ Ordinario_Tercer_Semestre/Modulo_Caballos.py
@@ -26,7 +26,7 @@ def imprimir_matriz(matriz_de_juego) -> None:
 
 
 
-def menu_de_interaccion():
+def menu_de_interaccion()->int:
     """
     Muestra el menú principal del juego donde el jugador elige qué hacer
     :return: Devuelve la opción seleccionada por el usuario
@@ -48,46 +48,56 @@ def menu_de_interaccion():
     return int(opcion)
 
 def reglas_de_juego ():
-
-    print("===========================================================================")
+    """
+    Esta función escribe en pantalla una pequeña informacion
+    de lo que trata el juego.
+    :return: No retorna nada
+    """
+    print("\n===========================================================================")
     print("--- Reglas Básicas del Juego de Carrera de Caballos con Baraja Española --- ")
     print("===========================================================================")
     print()
     print(">>>>>>> Objetivo del Juego")
     print("-El objetivo del juego es adivinar qué carta saldrá en la siguiente tirada.")
     print()
-    print(">>>>>>> Preparación del Juego ")
-    print("-Se baraja la baraja española de 48 cartas."
-          "\n-Se colocan las cartas boca abajo en una pila."
-          "\n-El jugador que juega primero es elegido al azar.")
-    print()
-    print(">>>>>>> Jugabilidad ")
-
+    print("Sacar una carta: El jugador saca una carta del montón en el centro de la mesa.")
+    print("Mover su caballo: El jugador puede mover su caballo una casilla hacia adelante o hacia atrás en función de la carta que sacó o lanzó.")
+    print("Si el caballo del jugador llega a la casilla final gana la partida")
 
 
 def juego_contra_cpu (diccionario_de_barajas):
+    """
+    En esta función se lleva a cabo toda la logica para hacer que los caballos corran
+    al levantar una carta y cuando pasen una sección de las ocultas, escoger una para
+    que un caballo se regrese. Se uso la implementación de matrices, diccionarios, listas y
+    cadenas.
+    :param diccionario_de_barajas: Es el diccionario que contiene toda la baraja española
+    :return: No retorna nada
+    """
     columnas_verficadas = []
     bandera = 0
     contador_escoger = 1
+    intentos = 4
 
-    matriz_de_juego = [["copa","    ", "    ", "    ", "M"], ["abasto", "    ", "    ", "    ", "E"], ["moneda", "    ", "    ", "    ", "T"], ["espada", "    ", "    ", "    ", "A"]]
+    matriz_de_juego = [["copa", "    ", "    ", "    ", "M"], ["abasto", "    ", "    ", "    ", "E"], ["moneda", "    ", "    ", "    ", "T"], ["espada", "    ", "    ", "    ", "A"]]
     print("\n\tComenzando carrera: \n")
     imprimir_matriz(matriz_de_juego)
+    print(f"Quedan {intentos} cartas ocultas")
 
-    print("\n->Escoge a que caballo apostaras:\n ")
+    print("\n->Escoge a qué caballo apostarás:\n ")
     for llave in diccionario_de_barajas.keys():
         print(f"[{contador_escoger}]", end="")
         print(f"Caballo: {llave}")
         contador_escoger += 1
 
-    caballo_usuario = input("Escribe la opcion que deceas: ")
+    caballo_usuario = input("Escribe la opción que deseas: ")
     while not caballo_usuario.isnumeric() or int(caballo_usuario) not in range (1, 5):
-        caballo_usuario = input("Error, intenta denuevo: ")
+        caballo_usuario = input("Error, intenta de nuevo: ")
     caballo_usuario = int(caballo_usuario)
 
     if caballo_usuario == 1:
         caballo_usuario = "moneda"
-    elif caballo_usuario ==2:
+    elif caballo_usuario == 2:
         caballo_usuario = "copa"
     elif caballo_usuario == 3:
         caballo_usuario = "espada"
@@ -97,9 +107,154 @@ def juego_contra_cpu (diccionario_de_barajas):
 
     caballo_Cpu = random.choice(list(diccionario_de_barajas.keys()))
 
-    print(f"\nTu apostaste por el caballo de {caballo_usuario}\nEl cp aposto al caballo de {caballo_Cpu}")
+    print(f"\nTu apostaste por el caballo de {caballo_usuario}\nEl cpu apostó al caballo de {caballo_Cpu}")
 
 
+    levantar = None
+    while levantar != 0:
+        # Selección aleatoria de carta
+        llave_random = random.choice(list(diccionario_de_barajas.keys()))
+        print("\nLa carta levantada es: ", llave_random)
+
+        # Manipulación de la matriz de juego
+        for renglones in range(0, 4):
+            for columnas in range(0, 4):  # Recorrer hasta la columna 4 (índice 4)
+                if matriz_de_juego[renglones][columnas] == llave_random:
+                    # Verificamos si la carta ha llegado a la meta
+                    if matriz_de_juego[renglones][columnas + 1] in ["M", "E", "T", "A"]:
+                        matriz_de_juego[renglones][columnas + 1] = llave_random
+                        matriz_de_juego[renglones][columnas] = "     "
+                        imprimir_matriz(matriz_de_juego)
+                        print(f"Quedan {intentos} cartas ocultas")
+                        print(f"{llave_random} ha ganado.")
+                        if llave_random == caballo_usuario:
+                            print("\nHas ganado la apuesta, felicidades")
+                        elif llave_random == caballo_Cpu:
+                            print("\nEl cpu ha ganado")
+                        else:
+                            print("\nEsto ha sido un empate, nadie ganó")
+                        levantar = 0  # Terminamos el juego
+                    else:
+                        # Verificación de columnas vacías
+
+                        for c in range(0, 5):  # iteramos hasta la columna 4
+                                if bandera == 1:
+                                    c=+1
+
+                                contador_columna = 0
+                                for r in range(0, 4):  # Iteramos por las filas
+                                    if matriz_de_juego[r][c] == "     ":
+                                        contador_columna += 1
+                                # Regresar cartas
+                                if contador_columna == 4:
+                                    bandera = 1
+                                    for rg in range(0, 4):
+                                        for cl in range(0, 5):
+                                            columnas_verficadas.append(matriz_de_juego[rg][cl])
+                                        break
+
+
+                                    print(f"La columna {c} tiene 4 celdas vacías.")
+                                    regresar_carta = random.choice(list(diccionario_de_barajas.keys()))
+
+                                    for clave, valor in diccionario_de_barajas.items():
+                                        if regresar_carta == clave:
+                                            numero_de_carta = random.choice(list(valor))
+                                            list(valor).remove(numero_de_carta)
+
+                                    print(f"\nLa carta que se regresará un espacio es {regresar_carta}")
+                                    print(f"La baraja levantada es: {numero_de_carta} de {regresar_carta}")
+                                    intentos -= 1
+                                    print(f"Quedan {intentos} cartas ocultas")
+
+
+                                    for rg in range(0, 4):
+                                        for cl in range(0, 5):
+
+                                            if matriz_de_juego[rg][cl] == regresar_carta:
+                                                matriz_de_juego[rg][cl] = "    "
+                                                matriz_de_juego[rg][cl - 1] = regresar_carta
+
+                                                imprimir_matriz(matriz_de_juego)
+                                                print(f"Quedan {intentos} cartas ocultas")
+                                    break
+
+
+                        if contador_columna != 4 :
+                            # Si no hay columnas vacías, se mueve la carta
+                            matriz_de_juego[renglones][columnas + 1] = llave_random
+                            matriz_de_juego[renglones][columnas] = "     "
+                            imprimir_matriz(matriz_de_juego)
+                            print(f"\nQuedan {intentos} cartas ocultas")
+                            break
+
+
+        if levantar != 0:
+            levantar = input("[1] Para levantar otra carta o [0] para terminar programa: ")
+            while not levantar.isnumeric() or int(levantar)>1:
+                levantar = input("Error ingresa de nuevo: ")
+
+            levantar = int(levantar)
+
+
+def dos_jugadores (diccionario_de_barajas):
+    """
+    En esta función se lleva a cabo toda la lógica para hacer que los caballos corran
+    al levantar una carta y cuando pasen una sección de las ocultas, escoger una para
+    que un caballo se regrese. Se uso la implementación de matrices, diccionarios, listas y
+    cadenas. Esta función es para que dos jugadores apuesten a un caballo.
+    :param diccionario_de_barajas: Diccionario que contiene las cartas de la baraja española
+    :return: No retorna nada
+    """
+    columnas_verficadas = []
+    bandera = 0
+    contador_escoger = 1
+    intentos = 4
+
+    matriz_de_juego = [["copa", "    ", "    ", "    ", "M"], ["abasto", "    ", "    ", "    ", "E"],
+                       ["moneda", "    ", "    ", "    ", "T"], ["espada", "    ", "    ", "    ", "A"]]
+
+    print("\n\tComenzando carrera: \n")
+    imprimir_matriz(matriz_de_juego)
+    print(f"Quedan {intentos} cartas ocultas")
+
+    print("\n->Escoge a qué caballo apostarás:\n ")
+    for llave in diccionario_de_barajas.keys():
+        print(f"[{contador_escoger}]", end="")
+        print(f"Caballo: {llave}")
+        contador_escoger += 1
+
+    caballo_usuario = input("Jugador 1: Escribe la opción que deseas: ")
+    while not caballo_usuario.isnumeric() or int(caballo_usuario) not in range(1, 5):
+        caballo_usuario = input("Error, intenta de nuevo: ")
+    caballo_usuario = int(caballo_usuario)
+
+
+    caballo_usuario2 = input("Jugador 2: Escribe la opción que deseas: ")
+    while not caballo_usuario2.isnumeric() or int(caballo_usuario) not in range(1, 5):
+        caballo_usuario2 = input("Error, intenta de nuevo: ")
+    caballo_usuario2 = int(caballo_usuario2)
+
+    if caballo_usuario == 1:
+        caballo_usuario = "moneda"
+    elif caballo_usuario == 2:
+        caballo_usuario = "copa"
+    elif caballo_usuario == 3:
+        caballo_usuario = "espada"
+    else:
+        caballo_usuario = "abasto"
+
+    if caballo_usuario2 == 1:
+        caballo_usuario2 = "moneda"
+    elif caballo_usuario2 == 2:
+        caballo_usuario2 = "copa"
+    elif caballo_usuario2 == 3:
+        caballo_usuario2 = "espada"
+    else:
+        caballo_usuario2 = "abasto"
+
+
+    print(f"\nTu apostaste por el caballo de {caballo_usuario}\nEl jugador 2 apostó al caballo de {caballo_usuario2}")
 
     levantar = None
     while levantar != 0:
@@ -118,68 +273,75 @@ def juego_contra_cpu (diccionario_de_barajas):
                         imprimir_matriz(matriz_de_juego)
                         print(f"{llave_random} ha ganado.")
                         if llave_random == caballo_usuario:
-                            print("\nHaz ganado la apuesta, felicidades")
-                        elif llave_random == caballo_Cpu:
-                            print("\nEl cpu ha ganado")
+                            print("\nEl jugador 1 ha ganado.")
+                        elif llave_random == caballo_usuario2:
+                            print("\nEl jugador 2 ha ganado")
                         else:
-                            print("\nEsto ha sido un empate, nadie gano")
+                            print("\nEsto ha sido un empate, nadie ganó")
                         levantar = 0  # Terminamos el juego
                     else:
                         # Verificación de columnas vacías
 
-                        for c in range(0, 5):  # Ahora iteramos hasta la columna 4
-                                if bandera == 1:
-                                    c=+1
+                        for c in range(0, 5):  #  iteramos hasta la columna 4
+                            if bandera == 1:
+                                c = +1
 
-                                contador_columna = 0
-                                for r in range(0, 4):  # Iteramos por las filas
-                                    if matriz_de_juego[r][c] == "     ":
-                                        contador_columna += 1
-
-                                if contador_columna == 4:
-                                    bandera=1
-                                    for rg in range(0, 4):
-                                        for cl in range(0, 5):
-                                            columnas_verficadas.append(matriz_de_juego[rg][cl])
-                                        break
-
-
-                                    print(f"La columna {c} tiene 4 celdas vacías.")
-                                    regresar_carta = random.choice(list(diccionario_de_barajas.keys()))
-                                    print(f"\nLa carta que se regresará un espacio es {regresar_carta}")
-
-                                    for rg in range(0, 4):
-                                        for cl in range(0, 5):
-
-                                            if matriz_de_juego[rg][cl] == regresar_carta:
-                                                matriz_de_juego[rg][cl] = "    "
-                                                matriz_de_juego[rg][cl - 1] = regresar_carta
-
-                                                imprimir_matriz(matriz_de_juego)
+                            contador_columna = 0
+                            for r in range(0, 4):  # Iteramos por las filas
+                                if matriz_de_juego[r][c] == "     ":
+                                    contador_columna += 1
+                            # Regresar carta
+                            if contador_columna == 4:
+                                bandera = 1
+                                for rg in range(0, 4):
+                                    for cl in range(0, 5):
+                                        columnas_verficadas.append(matriz_de_juego[rg][cl])
                                     break
 
+                                print(f"La columna {c} tiene 4 celdas vacías.")
+                                regresar_carta = random.choice(list(diccionario_de_barajas.keys()))
+                                for clave, valor in diccionario_de_barajas.items():
+                                    if regresar_carta == clave:
+                                        numero_de_carta = random.choice(list(valor))
+                                        list(valor).remove(numero_de_carta)
 
-                        if contador_columna != 4 :
+                                print(f"\nLa carta que se regresará un espacio es {regresar_carta}")
+                                print(f"La baraja levantada es: {numero_de_carta} de {regresar_carta}")
+                                intentos -= 1
+                                print(f"Quedan {intentos} cartas ocultas")
+
+                                for rg in range(0, 4):
+                                    for cl in range(0, 5):
+
+                                        if matriz_de_juego[rg][cl] == regresar_carta:
+                                            matriz_de_juego[rg][cl] = "    "
+                                            matriz_de_juego[rg][cl - 1] = regresar_carta
+
+                                            imprimir_matriz(matriz_de_juego)
+                                            print(f"Quedan {intentos} cartas ocultas")
+                                break
+
+                        if contador_columna != 4:
                             # Si no hay columnas vacías, se mueve la carta
                             matriz_de_juego[renglones][columnas + 1] = llave_random
                             matriz_de_juego[renglones][columnas] = "     "
                             imprimir_matriz(matriz_de_juego)
+                            print(f"Quedan {intentos} cartas ocultas")
                             break
-
 
         if levantar != 0:
             levantar = input("[1] Para levantar otra carta o [0] para terminar programa: ")
-            while not levantar.isnumeric() or int(levantar)>1:
-                levantar = input("Error ingresa denuevo: ")
+            while not levantar.isnumeric() or int(levantar) > 1:
+                levantar = input("Error ingresa de nuevo: ")
 
             levantar = int(levantar)
 
 
-
 def menu_principal ()->None:
     """
-
-    :return:
+    Menu donde se despliegan las acciones a ejecutar, dependiendo de la opcion que recibamos
+    de la función del menú de interacción
+    :return: Sin retornos
     """
 
     diccionario_de_barajas = {'moneda':[1, 2, 3, 4, 5, 6, 7, 10, 11],
@@ -190,21 +352,21 @@ def menu_principal ()->None:
     for clave, valor in diccionario_de_barajas.items():
         print(f"Clave: {clave} y  valor: {valor}")
 
-    #Despliega menu de iteraccion.
+    # Despliega menú de interacción.
     opcion = None
     while opcion != 0:
         opcion = menu_de_interaccion()
-        if opcion>=0 and opcion<4:
+        if opcion >= 0 and opcion < 4:
             if opcion == 0:
-                print("Salida Exitosa. Adios :)")
+                print("Salida Exitosa. Adiós :)")
             if opcion == 1:
                 reglas_de_juego()
             if opcion == 2:
                 juego_contra_cpu(diccionario_de_barajas)
             if opcion == 3:
-                reglas_de_juego()
+                dos_jugadores(diccionario_de_barajas)
         else:
-            print("Error inrtenta denuevo")
+            print("Error, intenta de nuevo")
 
 if __name__ == '__main__':
     menu_principal()
